cbf3af1b62f670a6ab2bd010d0c3cdc82d27c733
Joanna Chmielewska <joanna.chmielewska@wipro.com>
2019-08-08 12:29:47 +0200

    remove unused parameters

--- initValues.sh
+++ initValues.sh
@@ -2,12 +2,8 @@
 
 # Parameters - please enter correct values
 
-stage="prod"
 region="us-east-1"
-domain="buildit.digital"
 customDomain="feedback-survey-prod.buildit.digital"
-#You can check your certificate for domain here: https://console.aws.amazon.com/acm/
-certificateArn="arn:aws:acm:us-east-1:..."
 
 #aws_access_key_id:aws_secret_access_key
 awsKey="aws_access_key_id:aws_secret_access_key"
